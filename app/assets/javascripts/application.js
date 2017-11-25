a7a76024e149e1b020e3c4df53dc020971013ee2
Egor <egor.uxanov.97@mail.ru>
2017-11-25 03:21:26 -0500

    dsd

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,74 +11,14 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require_tree .
+
 //= require bootstrap-sprockets
 //= require trix
 //= require jquery
 //= require jquery_ujs
+//= require_tree
 
 
-
-(function ($) {
-    $.fn.fnMenu = function (options) {
-        var defaults = {
-           container: '.container',
-		   btn_menu: '.menu_btn',
-		   podl: '.podl', 
-		   width: '60%'
-        };
-        var settings = $.extend(defaults, options);
-		
-		
-		this.each(function() {
-			var elem = $(this);
-			new nikitaMenu(elem, settings)
-			
-		});
-		
-        return this;
-    }
-	
-	
-	function nikitaMenu(elem, settings) {
-		this.open = false;
-		var self = this;
-		$container = elem.find(settings.container);
-		$btn_menu = elem.find(settings.btn_menu);
-		$podl = elem.find(settings.podl);
-		/*что скажете на счет этого, строка 29 и 30? По другому я не придумал как сделать более гибко */
-		this.left = '-' + settings.width;
-		$container.css('left', this.left).css('top', '0').css('width', settings.width);
-		/*что скажете на счет этого ? По другому я не придумал как сделать более гибко */
-		elem.on('click', '.menu_btn, .podl', function() {
-			
-			if(!self.open) {
-				$container.css('display', 'block');
-				$container.animate({
-					left:0
-				},200, function() {
-					self.open = true;
-				});
-				$podl.css('display', 'block').animate({
-					opacity:0.3
-				}, 150);
-			} else {
-				$container.animate({
-					left: self.left
-				},200, function() {$container.css('display', 'block'); self.open = false;});
-				$podl.animate({
-					opacity:0
-				}, 150, function() {
-					$podl.css('display', 'none');
-				});
-			}
-
-		});
-		
-	}
-	
-	
-})(jQuery);
 
 
 
@@ -91,32 +31,10 @@
  */
 
 
-$(function () {
-    $('.menuS').fnMenu();
-});
 
 
 
 
-//Якорь 
 
-$(function() {
- $(window).scroll(function() {
- if($(this).scrollTop() != 0) {
-$('#toTop').fadeIn();
-} else {
-$('#toTop').fadeOut();
- }
-});
-$('#toTop').click(function() {
- $('body,html').animate({scrollTop:0},800);
-});
- });
-
-$(function(){
-		$('.menu').slicknav({
-      label: ''
-    });
-});
 
 
