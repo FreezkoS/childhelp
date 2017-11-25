ab4fca0a6db9087b1b5ff2672a2e71cc0dd32fc9
Egor <egor.uxanov.97@mail.ru>
2017-11-25 00:09:29 -0500

    sdsd

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+    
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -41,7 +42,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
+config.assets.compile = true # было false,но добавил true для компиляции файла css
+ config.serve_static_files = true 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
