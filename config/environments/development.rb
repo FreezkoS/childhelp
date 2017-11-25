787b674102d7d42379c84f0801a0c66d2ae77fb1
Egor <egor.uxanov.97@mail.ru>
2017-11-25 00:17:09 -0500

    sdsd

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,8 +42,6 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-config.assets.compile = true # было false,но добавил true для компиляции файла css
- config.serve_static_files = true 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
